9e3aab614c43561f46ed72c7b6d02f7d7d38499d
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-04-23 18:56:33 -0400

    rendering comments from data

--- app/components/CommentBox.jsx
+++ app/components/CommentBox.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 var marked = require('marked');
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var Comment = React.createClass({
   rawMarkup: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
@@ -22,10 +27,17 @@ var Comment = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -46,7 +58,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -54,23 +66,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
-
-
-//var CommentBox = React.createClass({
-//  displayName: 'CommentBox',
-//
-//  render: function() {
-//    return (
-//      React.createElement('div', {className: "commentBox"},
-//        "Hello, world! I am a CommentBox."
-//      )
-//    );
-//  }
-//});
-//ReactDOM.render(
-//  React.createElement(CommentBox, null),
-//  document.getElementById('content')
-//);
