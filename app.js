32913b09c1a2f13a87e0e0a70e3357ff0a5c603f
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-04-24 15:20:23 -0400

    separating react components into different files

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import Hello from './app/components/hello.jsx';
-import World from './app/components/world.jsx';
-import CommentBox from './app/components/CommentBox.jsx';
-
 require("./app/assets/css/my-style.css");
+
+import CommentBox from './app/components/CommentBox.jsx';
